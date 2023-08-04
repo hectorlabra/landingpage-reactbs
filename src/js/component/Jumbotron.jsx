47955736e582d1d41f1591814bab4a13ebceeaae
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-04 22:04:50 +0000

    jumbotron fixes

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Jumbotron = () => {
   return (
-    <div className="jumbotron jumbotron-fluid bg-primary text-light text-center" id="home">
+    <div className="jumbotron jumbotron-fluid bg-primary text-light text-left py-5" id="home">
       <div className="container">
         <h1 className="display-4">Welcome to Our Website</h1>
         <p className="lead">We provide amazing services to our clients.</p>
