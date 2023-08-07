ed894eeee1c1b46f9ca2c1a33edac8856baa68ef
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-07 20:40:21 +0000

    navbar bs css fixes

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+      {/* Agregamos la clase sticky-top */}
       <div className="container">
         <a className="navbar-brand" href="/">
           Start Bootstrap
